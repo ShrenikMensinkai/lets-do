8d61a749fcee5d69abc2122bbd51249d93a0780e
shrenik.mensinkai <shrenik.mensinkai@letstransport.team>
2021-02-06 21:30:20 +0530

    user registration email template added

--- server/util/config-mail.js
+++ server/util/config-mail.js
@@ -1,17 +1,19 @@
 const sgMail = require('@sendgrid/mail');
-
+const path = require('path');
+const ejs = require('ejs');
 class Email{
     async sendRegistrationMail(mailId, inviteLink){
         try{
+            let email_template= await ejs.renderFile(path.join(__dirname, '/register-email.ejs'),{
+                confirm_link: inviteLink
+            });
             let email = mailId.trim();
             sgMail.setApiKey(process.env.EMAIL_STRING);
             let msg = {
                 to: email,
                 from: 'shrenik.msk@gmail.com',
-                subject: 'User Registration',
-                text: 'login',
-                html: `<p>Link - <strong>${inviteLink}</strong>. 
-                Use this password to login and reset the password</p>`,
+                subject: 'Team FLF',
+                html: email_template
             };
             sgMail.send(msg).then(() => {
                 console.log('Email sent')
